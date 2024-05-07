1b72c05a44ddefb72c16873a3ac964b9d0fd4033
chk03042010 Chang <huakeatchang@gmail.com>
2024-05-07 13:36:02 +0000

    Put Student Demerit Marks and Merit Demerit Record s js file into a folder

--- app/Marks/page.js
+++ app/Marks/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react';
-import Login from './auth/page';
+import Login from '../auth/page';
 
 const GuestType = 'User'
 const details = [{
