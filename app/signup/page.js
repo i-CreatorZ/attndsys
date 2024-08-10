6f0ed943c3971f645dec1f84f11aa6a09709a224
chk03042010 <166121795+chk03042010@users.noreply.github.com>
2024-08-10 09:40:39 +0000

    fixed bugs

--- app/signup/page.js
+++ app/signup/page.js
@@ -11,6 +11,7 @@ export default function Signup() {
   const [name, setName] = useState('')
   const [kelas, setClass] = useState('')
   const [userExistData, setUserExist] = useState(false)
+  const [passCorrect, setPassCorrect] = useState(true)
   const router = useRouter()
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,10 +20,12 @@ export default function Signup() {
       .from('member_info')
       .select()
       .eq('id', id)
-      if (data.id == id){
+      .single()
+      console.log("data: ", data)
+      console.log("id: ", id)
+      if (data && data.id === id){
         setUserExist(true)
       }
-      else{
       const hash = bcrypt.hashSync(password, 10)
       const { data:data2, error:error2 } = await supabase
       .from('member_info')
@@ -32,6 +35,8 @@ export default function Signup() {
       .select()
       router.push(`/Marks?id=${id}&type=${"User"}`)
     }
+    else{
+      setPassCorrect(false)
     }
   }
   return (
@@ -67,6 +72,7 @@ export default function Signup() {
                       <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                       <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required onChange={(e) =>{setComfirm(e.target.value)}}/>
                   </div>
+                  <p>{!passCorrect ? "Confirm Password Incorrect": ""}</p>
                   <p>{userExistData ? "This user is already registered":""}</p>
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
               </form>
