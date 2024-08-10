db04b4cce993e09f5675880c50ae95549eb7e3a4
chk03042010 <166121795+chk03042010@users.noreply.github.com>
2024-08-10 08:35:53 +0000

    Sign in and Sign up

--- app/Marks/page.js
+++ app/Marks/page.js
@@ -1,174 +1,188 @@
 'use client'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react';
-import {supabase} from '../supabase.js'
-import {useRouter} from 'next/navigation'
-const GuestType = 'Admin'
-const schoolNum = "D5001"
-function Authentication() {
-  const [member_Details ,setDetails]  = useState([])
 
-if (GuestType === 'User') {
-  useEffect(() =>{
-    const fetchMemberData = async ()=>{
-      const {data, error} = await supabase
-        .from("member_info")
-        .select()
-        .eq("id", schoolNum)
-        .single()
-      if (error){
-        console.log("Member marks error: " + error)
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../supabase.js';
+import { useSearchParams } from 'next/navigation';
+
+
+function UserType() {
+  const router = useRouter()
+  const [member_Details, setDetails] = useState([]);
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
+  const type = searchParams.get('type')
+  console.log("type: ", type)
+  console.log("id: ", id)
+  useEffect(() => {
+    const fetchMemberData = async () => {
+      if (type === 'User') {
+        const { data, error } = await supabase
+          .from("member_info")
+          .select()
+          .eq("id", id)
+          .single();
+
+        if (error) {
+          console.log("Member marks error: " + error);
+        } else if (data) {
+          setDetails(data);
+        }
+      } else {
+        const fetchAllMemberData = async () => {
+          const { data, error } = await supabase
+            .from("member_info")
+            .select();
+
+          if (error) {
+            console.log("Member marks error: " + error);
+          } else if (data) {
+            setDetails(data);
+          }
+        };
+        fetchAllMemberData();
       }
-      else if(data){
-        setDetails(data)
-      }
-    }
-    fetchMemberData()
-  })
-  return (
-    <tr key= {member_Details.id} >
-      <td>{member_Details.id}</td>
-      <td>{member_Details.name}</td>
-      <td>{member_Details.class}</td>
-      <td>{member_Details.merit}</td>
-    </tr>
-  );
+    };
+    fetchMemberData();
+  }, [id, type]);
+
+  if (type === 'User') {
+    return (
+      <tr key={member_Details.id}>
+        <td>{member_Details.id}</td>
+        <td>{member_Details.name}</td>
+        <td>{member_Details.class}</td>
+        <td>{member_Details.merit}</td>
+      </tr>
+    );
+  } else {
+    const detailsList = member_Details.map(person => (
+      <tr key={person.id}>
+        <td>{person.id}</td>
+        <td>{person.name}</td>
+        <td>{person.class}</td>
+        <td>{person.merit}</td>
+        <td>
+          <Link
+            href={{
+              pathname: '/Merit',
+              query: { id: person.id, name: person.name },
+            }}
+          >
+            Merit
+          </Link>
+        </td>
+        <td>
+          <Link
+            href={{
+              pathname: '/Demerit',
+              query: { id: person.id, name: person.name },
+            }}
+          >
+            Demerit
+          </Link>
+        </td>
+      </tr>
+    ));
+    return detailsList;
+  }
 }
-else {
-  useEffect(() =>{
-    const fetchAllMemberData = async ()=>{
-      const {data, error} = await supabase
-        .from("member_info")
-        .select()
-  
-      if (error){
-        console.log("Member marks error: " + error)
-      }
-      else if(data){
-        setDetails(data)
-      }
-    }
-    fetchAllMemberData()
-  }),[]
-  const detailsList = member_Details.map(person =>
-    <tr key={person.id}>
-      <td>{person.id}</td>
-      <td>{person.name}</td>
-      <td>{person.class}</td>
-      <td>{person.merit}</td>
-      <td><Link
-      href = {{
-        pathname:'/Merit',
-        query:{id:person.id, name: person.name}
-      }}
-      >Merit</Link></td>
-      <td><Link
-      href = {{
-        pathname:'/Demerit',
-        query:{id:person.id, name: person.name}
-      }}
-      >Demerit</Link></td>
-    </tr>
-  )
-  return (
-    detailsList
-  )
-}
-}
-function Records({database}) {
+
+function Records({ database }) {
   const [records, setRecords] = useState([]);
   const [fetchError, setFetchError] = useState(null);
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
 
   useEffect(() => {
     const fetchRecords = async () => {
-        const { data, error } = await supabase
-          .from(database)
-          .select()
-          .eq('school_num', schoolNum);
+      const { data, error } = await supabase
+        .from(database)
+        .select()
+        .eq('school_num', id);
 
-        if (error) {
-          console.error("Error fetching records:", error);
-          setFetchError(error);
-          setRecords(null)
-        } 
-        else if (data) {
-          setRecords(data);
-          setFetchError(null)
-        }
-      } 
-      fetchRecords();
-    })
-   ,[schoolNum];
-const recordList = records.map(record => 
-  <tr key={record.id}>
-    <td>{record.date}</td>
-    <td>{record.reason}</td>
-    <td>{record.mark}</td>
-  </tr>
-)
-  return (
-    recordList
-  )
+      if (error) {
+        console.error("Error fetching records:", error);
+        setFetchError(error);
+        setRecords(null);
+      } else if (data) {
+        setRecords(data);
+        setFetchError(null);
+      }
+    };
+    fetchRecords();
+  }, [id, database]);
+
+  const recordList = records.map(record => (
+    <tr key={record.id}>
+      <td>{record.date}</td>
+      <td>{record.reason}</td>
+      <td>{record.mark}</td>
+    </tr>
+  ));
+
+  return recordList;
 }
-
 
 function ShowRecord() {
+  const searchParams = useSearchParams()
+  const type = searchParams.get('type')
 
   return (
-    GuestType === 'User' &&
-    <div>
-      <h2 className="flex justify-center">Demerit Records</h2>
-      <table className="table-auto border-separate border-spacing-x-5">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Reason</th>
-            <th>Marks Deducted</th>
-          </tr>
-        </thead>
-        <tbody>
-        <Records database = "demerit_records"></Records>
-        </tbody>
-      </table>
-      <h2 className="flex justify-center mt-10">Merit Records</h2>
-      <table className="table-auto border-separate border-spacing-x-5">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Reason</th>
-            <th>Marks Added</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <Records database= "merit_records"></Records>
-        </tbody>
-      </table>
-    </div>
-  )
+    type === 'User' && (
+      <div>
+        <h2 className="flex justify-center">Demerit Records</h2>
+        <table className="table-auto border-separate border-spacing-x-5">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Reason</th>
+              <th>Marks Deducted</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Records database="demerit_records" />
+          </tbody>
+        </table>
+        <h2 className="flex justify-center mt-10">Merit Records</h2>
+        <table className="table-auto border-separate border-spacing-x-5">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Reason</th>
+              <th>Marks Added</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Records database="merit_records" />
+          </tbody>
+        </table>
+      </div>
+    )
+  );
 }
 
-export default function Home() {
-  console.log(supabase)
-    return (
-      <><main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <table className="table-auto border-separate border-spacing-x-5">
-            <thead>
-              <tr>
-                <th>School Number</th>
-                <th>Name</th>
-                <th>Class</th>
-                <th>Marks</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <Authentication />
-            </tbody>
-          </table>
-          <ShowRecord />
-        </main></>
-    )
-  }
+export default function Marks() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <table className="table-auto border-separate border-spacing-x-5">
+        <thead>
+          <tr>
+            <th>School Number</th>
+            <th>Name</th>
+            <th>Class</th>
+            <th>Marks</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <UserType />
+        </tbody>
+      </table>
+      <ShowRecord />
+    </main>
+  );
+}
+
--- app/auth/page.js
+++ app/auth/page.js
@@ -1,101 +1,78 @@
-"use client"
-// import { useState } from "react"
-// import { supabase } from "../supabase.js"
-// export default function Login() {
-//     async function signInWithEmail() {
-//         const { data, error } = await supabase.auth.signInWithPassword({
-//             email: email,
-//             password: password,
-//         })
-//     }
-//     const [email, setEmail] = useState("")
-//     const [password, setPassword] = useState("")
-
-//     const  handleChange = (event) => {
-// 		setEmail(event.target.value);
-//         setPassword(event.target.value);
-// 	};
-//     return (
-//         <div>
-//             <form>
-//                 <label>Input Value:
-//                     <input type="text" value={setEmail} onChange={handleChange} />
-//                     <input type="text" value={setPassword} onChange={handleChange} />
-//                 </label>
-//                 <p>Input Value: {email}</p>
-//             </form>
-//         </div>
-
-//     );
-// }
-
-
-
-import { useState } from 'react';
-import { supabase } from "../supabase.js";
+'use client'
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { supabase } from "../supabase.js";
+import bcrypt from 'bcryptjs';
 
 export default function Login() {
   const router = useRouter();
-  const [id, setid] = useState('');
+  const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [userData, setUserData] = useState(true);
 
   const handleLogin = async (e) => {
     e.preventDefault();
-  try{
     const { data, error } = await supabase
       .from("member_info")
-      .select('id,  password')
+      .select('id, password, type')
       .eq('id', id)
       .single();
 
     if (error) {
-      throw error;
+      console.log(error);
+      setUserData(false);
+      return;
     }
 
-    // If user not found
-    if (!data) {
-      throw new Error('User not found');
-    }
-
-    // Compare passwords
-    if (data.password === password) {
-      // Passwords match, redirect to home page
-      router.push('/Home'); // Adjust the route as per your setup
+    if (data) {
+      const isPasswordCorrect = bcrypt.compareSync(password, data.password);
+      console.log(isPasswordCorrect);
+      if (isPasswordCorrect) {
+        // Passwords match, redirect to /Marks with id and type as query parameters
+        router.push(`/Marks?type=${data.type}&id=${id}`);
+        }
+       else {
+        setUserData(false);
+      }
     } else {
-      throw new Error('Invalid password');
+      setUserData(false);
     }
-  } catch (error) {
-    console.error('Login error:', error.message);
-    // Handle login error (e.g., display error message)
-  }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="id">ID:</label>
-          <input
-            id="id"
-            value={id}
-            onChange={(e) => setid(e.target.value)}
-            required
-          />
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+          Dashboard    
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your dashboard
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleLogin}>
+              <div>
+                <label htmlFor="id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your ID</label>
+                <input type="id" name="id" id="id" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="D0001" value={id} required onChange={(e) => setId(e.target.value)} />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <p className="text-danger">{!userData ? "The ID or password is incorrect!" : ""}</p>
+                </div>
+              </div>
+              <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+            </form>
+          </div>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 }
+
+
+
